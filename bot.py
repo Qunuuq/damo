05fa2101b36a7d1cf1e2956a16eccbde688dcd13
yyqqttwwre <157679909+yyqqttwwre@users.noreply.github.com>
2024-06-15 06:29:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,38 @@
-from pyrogram import Client, filters
-from mmllm.her import app
+from pyrogram import Client
+
+from pyrogram import filters
+
+from pyrolistener import Listener
 
 
+
+api_id = 20220894
+
+api_hash = "87b472b166bd80d7420df36147783a37"  
+
+bot_token = "6739169902:AAEjKSq3UQw0JXFY2tJTPdQsmE8Pyb77tNA"
+
+print_id = 5207032121 ##ايدي تحويل نقاط
+
+
+
+app = Client(
+
+    "MyApp",
+
+    api_id=api_id,
+
+    api_hash=api_hash,
+
+    bot_token=bot_token,
+
+    plugins=dict(root="plugins")
+
+)
+
+
+
+
+
+listener = Listener(client=app, show_output=True)
 app.run()
